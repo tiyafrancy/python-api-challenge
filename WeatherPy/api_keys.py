69af4de0339e358c265640341aa9e3eea04f2a6d
Tiya Francy <tiyafrancy87@gmail.com>
2025-02-05 12:40:17 -0600

    Module 6 challenge

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "3dd45f7c15975a5404bd94727e219ec3"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "25254347165f4067b3677eb4dd12fabf"
